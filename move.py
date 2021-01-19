debb7966770505781126357bc012a187bbe0ae52
LenWinkler <lenwinkler86@gmail.com>
2021-01-18 21:58:59 -0500

    moved TODO list to the README

--- move.py
+++ move.py
@@ -1,15 +1,5 @@
-# TODOS:
-# I want to be able to:
-# --point a scraper at a url for a certain system (e.g. nes) on a rom site and
-#   have it download all the roms for that system (paginated, many pages/files)
-# --once they're downloaded, send a copy of the zip file to a "backup" folder
-#   and unzip all the files
-# --then, go through all the unzipped folders, grab each rom file, copy the
-#   name (without extension) to an "inventory" file, and copy the rom to the
-#   appropriate folder on a flash drive
-# --finally, delete zip files (outside of "backup") and unzipped folders
-#   (could maybe do this right after rom is copied to flash drive)
-
+# start of a fn that will move the unzipped files to the appropriate folders
+# on the flash drive
 
 import os
 
