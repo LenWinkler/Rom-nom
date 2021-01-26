8be3546056393657f8d61d372b5b6cfb8bee19fe
LenWinkler <lenwinkler86@gmail.com>
2021-01-25 21:11:38 -0500

    switch to selenium. grabbing td tags and pulling out hrefs

--- traverse_roms.py
+++ traverse_roms.py
@@ -1,6 +1,21 @@
-import requests, bs4
-import get_dl_link from get_dl_link
+# import requests, bs4
+# import get_dl_link from get_dl_link
 
-def traverse_roms(url):
-    res = requests.get(url)
-    res.raise_for_status()
+# def traverse_roms(url):
+#     res = requests.get(url)
+#     res.raise_for_status()
+
+# bs4 doesn't work since site uses JS to start dl. switching to selenium
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+# grab all td tags, then put the url for each title into a list
+td_tags = driver.find_elements_by_tag_name('td')
+links = []
+for td in td_tags:
+    try:
+        links.append(td.find_element_by_tag_name('a').get_attribute('href'))
+    except:
+        continue
