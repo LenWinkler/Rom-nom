e63e4c88ddd6ffe443ba46581655999505f7598e
LenWinkler <lenwinkler86@gmail.com>
2021-01-29 18:10:35 -0500

    rename traverse_roms to download. download_roms now grabs link to each dl page

---
+++ download.py
@@ -0,0 +1,32 @@
+# import requests, bs4
+# import get_dl_link from get_dl_link
+
+# def traverse_roms(url):
+#     res = requests.get(url)
+#     res.raise_for_status()
+
+# bs4 doesn't work since site uses JS to start dl. switching to selenium
+
+from selenium import webdriver
+import time
+
+def download_roms(url):
+    driver = webdriver.Firefox()
+    print('opening url...')
+    time.sleep(3)
+    driver.get('https://romsmania.cc/roms/nintendo')
+    print('grabbing links to dl pages...')
+    time.sleep(3)
+
+    # grab all td tags, then put the url for each title into a list
+    td_tags = driver.find_elements_by_tag_name('td')
+    links = []
+    for td in td_tags:
+        try:
+            links.append(td.find_element_by_tag_name('a').get_attribute('href'))
+        except:
+            continue
+
+    print(links)
+
+download_roms('asdf')
