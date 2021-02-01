6b2d37ed7490de5c9903b47ad6ae8f9b92615631
LenWinkler <lenwinkler86@gmail.com>
2021-01-31 21:59:18 -0500

    add helper to create chrome instance with custom download path and download_roms now downloads all roms from a single page

--- download.py
+++ download.py
@@ -1,12 +1,26 @@
 from selenium import webdriver
-import time
+import time, os
+
+# helper function that creates a chrome instance with custom download path
+def new_chrome_browser():
+    options = webdriver.ChromeOptions()
+    download_path = '/Users/lenwinkler/Desktop/RETROPIE/ROMS/NES'
+    os.makedirs(download_path, exist_ok=True)
+
+    prefs = {}
+    prefs['profile.default_content_settings.popups']=0
+    prefs['download.default_directory']=download_path
+    options.add_experimental_option("prefs", prefs)
+    browser = webdriver.Chrome(options=options, executable_path='/Users/lenwinkler/Downloads/chromedriver')
+
+    return browser
 
 def download_roms(url):
-    driver = webdriver.Chrome()
+    driver = new_chrome_browser()
     print('opening url...')
     time.sleep(3)
     driver.get('https://romsmania.cc/roms/nintendo')
-    print('grabbing links to dl pages...')
+    print('grabbing links to download pages...')
     time.sleep(3)
 
     # grab all td tags, then put the url for each title into a list
@@ -30,6 +44,6 @@ def download_roms(url):
         driver.get(links[i])
         dl_button = driver.find_elements_by_class_name('btn__right')[0]
         dl_button.click()
-        time.sleep(10)
+        time.sleep(4)
 
 download_roms('asdf')
