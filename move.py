0ed998966bbeddb74ece22cbd9e54c185a43181c
LenWinkler <lenwinkler86@gmail.com>
2021-02-02 01:25:19 -0500

    move.py unzips all zip files to current dir

--- move.py
+++ move.py
@@ -1,20 +1,27 @@
 # start of a fn that will move the unzipped files to the appropriate folders
 # on the flash drive
 
-import os, shutil
+import os, shutil, zipfile
 
 def move_roms(dl_dir, dest_dir):
 
     os.chdir(r'/Users/lenwinkler/Desktop/RETROPIE/ROMS/NES')
+    cwd = os.getcwd()
     os.makedirs('zip_files', exist_ok=True)
 
-    for zipfile in os.listdir(os.getcwd()):
-        if zipfile.startswith('.') or not zipfile.endswith('.zip'):
+    if not os.path.isfile(cwd + '/inventory.txt'):
+        inventory = open('inventory.txt', 'a')
+        inventory.close()
+
+    for zip_file in os.listdir(cwd):
+        if zip_file.startswith('.') or not zip_file.endswith('.zip'):
             continue
         
-        shutil.copy(zipfile, os.getcwd() + '/zip_files')
-        
-        
+        shutil.copy(zip_file, cwd + '/zip_files')
+
+        with zipfile.ZipFile(os.path.abspath(zip_file), 'r') as zip_ref:
+            print(zip_file)
+            zip_ref.extractall(cwd)
 
         # print('\n' + '*' * 10)
         # print(folder)
