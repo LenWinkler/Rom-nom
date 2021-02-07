8f84ba80fc31313146fc4337416ffdbae2248f2e
LenWinkler <lenwinkler86@gmail.com>
2021-02-07 12:24:37 -0500

    download should now loop over all pages in a category

--- download.py
+++ download.py
@@ -23,34 +23,49 @@ def download_roms(url):
     print('grabbing links to download pages...')
     time.sleep(3)
 
-    # grab all td tags, then put the url for each title into a list
-    td_tags = driver.find_elements_by_tag_name('td')
-    links = []
-    for td in td_tags:
+    should_continue = True
+    while should_continue:
+        # grab all td tags, then put the url for each title into a list
+        td_tags = driver.find_elements_by_tag_name('td')
+        links = []
+        for td in td_tags:
+            try:
+                links.append(td.find_element_by_tag_name('a').get_attribute('href'))
+            except:
+                continue
+
+        print('opening tabs...')
+        for i in range(len(links)):
+            driver.execute_script("window.open('');")
+
+        tabs = driver.window_handles[1:]
+
+        print('starting downloads...\n')
+        for i in range(len(tabs)):
+            driver.switch_to.window(tabs[i])
+            driver.get(links[i])
+            dl_button = driver.find_elements_by_class_name('btn__right')[0]
+            rom_name = driver.find_element_by_xpath("//h1[@itemprop='name']").text
+            # for NES, if we hit the 'ZZZ' roms, quit
+            if rom_name.startswith('ZZZ'):
+                should_continue = False
+                break
+            dl_button.click()
+            print(f'downloading {rom_name}')
+            time.sleep(4)
+
+        print('closing tabs...')
+        for i in range(len(tabs)):
+            driver.switch_to.window(tabs[i])
+            driver.close()
+
         try:
-            links.append(td.find_element_by_tag_name('a').get_attribute('href'))
+            next_page = driver.find_element_by_xpath("//a[@title='Next page']")
         except:
+            should_continue = False
             continue
 
-    print('opening tabs...')
-    for i in range(len(links)):
-        driver.execute_script("window.open('');")
-
-    tabs = driver.window_handles[1:]
-
-    print('starting downloads...\n')
-    for i in range(len(tabs)):
-        driver.switch_to.window(tabs[i])
-        driver.get(links[i])
-        dl_button = driver.find_elements_by_class_name('btn__right')[0]
-        rom_name = driver.find_element_by_xpath("//h1[@itemprop='name']").text
-        dl_button.click()
-        print(f'downloading {rom_name}')
-        time.sleep(4)
-
-    print('closing tabs...')
-    for i in range(len(tabs)):
-        driver.switch_to.window(tabs[i])
-        driver.close()
+        driver.execute_script("arguments[0].scrollIntoView(true);", next_page)
+        next_page.click()
 
 download_roms('asdf')
