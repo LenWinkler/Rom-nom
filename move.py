6822db912b333ee3a950e96e4d9fccc5a2e1b6c7
LenWinkler <lenwinkler86@gmail.com>
2021-02-12 01:44:31 -0500

    remove hard coded dirs from move. change how inventory is built

--- move.py
+++ move.py
@@ -1,20 +1,19 @@
 import os, shutil, zipfile, time
 
-def move_roms(dl_dir, dest_dir):
+def move_roms(move_from, move_to):
     print('starting move...\n')
     start = time.perf_counter()
-    os.chdir(r'/Users/lenwinkler/Desktop/RETROPIE/ROMS/NES')
+    os.chdir(move_from)
     cwd = os.getcwd()
-    dst_dir = (r'/Volumes/UUI')
+    dst_dir = (move_to)
     os.makedirs('zip_files', exist_ok=True)
 
     if not os.path.isfile(fr'{cwd}/inventory.txt'):
         inventory = open('inventory.txt', 'a')
         inventory.close()
 
-    if not os.path.isdir(fr'{dst_dir}/NES'):
-        os.makedirs(fr'{dst_dir}/NES')
-    dst_dir = fr'{dst_dir}/NES'
+    if not os.path.isdir(move_to):
+        os.makedirs(move_to)
 
     inventory_list = []
 
@@ -31,16 +30,14 @@ def move_roms(dl_dir, dest_dir):
         
 
         os.remove(zip_file)
-    sort_start = time.perf_counter()
+
     inventory_list.sort()
-    sort_end = time.perf_counter()
-    print(f'sorting completed in {sort_end - sort_start:0.2f} seconds\n')
 
     print('building inventory file...\n')
-    for title in inventory_list:
-        with open('inventory.txt', 'a') as inventory:
+    with open('inventory.txt', 'a') as inventory:
+        for title in inventory_list:
             inventory.write(title + '\n')
-            inventory.close()
+        inventory.close()
 
     print('copying files to flash drive...\n')
     for item in os.listdir(cwd):
@@ -52,5 +49,3 @@ def move_roms(dl_dir, dest_dir):
 
     end = time.perf_counter()
     print(f'move completed in {end - start:0.2f} seconds\n')
-
-move_roms('dl_dir', 'dest_dir')
