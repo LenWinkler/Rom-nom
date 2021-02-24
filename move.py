1dcca2a0841913b3eff76c7d086be724ebefd3e3
LenWinkler <lenwinkler86@gmail.com>
2021-02-23 22:10:32 -0500

    inventory is now built in download

--- move.py
+++ move.py
@@ -8,14 +8,8 @@ def move_roms(move_from, move_to):
     dst_dir = (move_to)
     os.makedirs('zip_files', exist_ok=True)
 
-    if not os.path.isfile(fr'{cwd}/inventory.txt'):
-        inventory = open('inventory.txt', 'a')
-        inventory.close()
-
     if not os.path.isdir(move_to):
         os.makedirs(move_to)
-
-    inventory_list = []
 
     for zip_file in os.listdir(cwd):
         if zip_file.startswith('.') or not zip_file.endswith('.zip'):
@@ -26,18 +20,7 @@ def move_roms(move_from, move_to):
         with zipfile.ZipFile(os.path.abspath(zip_file), 'r') as zip_ref:
             zip_ref.extractall(cwd)
 
-        inventory_list.append(zip_file[:-4])
-        
-
         os.remove(zip_file)
-
-    inventory_list.sort()
-
-    print('building inventory file...\n')
-    with open('inventory.txt', 'a') as inventory:
-        for title in inventory_list:
-            inventory.write(title + '\n')
-        inventory.close()
 
     print('copying files to flash drive...\n')
     for item in os.listdir(cwd):
